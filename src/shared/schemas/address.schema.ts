c8b5210a9190d18934f6d0a4feea8e634d045ea2
Igson Félix <igson660@gmail.com>
2026-08-11 12:06:20 -0300

    Update address.schema.ts

--- src/shared/schemas/address.schema.ts
+++ src/shared/schemas/address.schema.ts
@@ -4,7 +4,7 @@ export const addressEmbeddedSchema = z.object({
   zip_code: z.string().min(8, "CEP obrigatório"),
   address: z.string().min(3, "Endereço obrigatório"),
   address_number: z.string().min(1, "Número obrigatório"),
-  address_complement: z.string().max(20, "Maximo 20 caracteres"),
+  address_complement: z.string().max(50, "Maximo 20 caracteres"),
   state: z.string().length(2, "UF inválida"),
   city: z.string().min(2, "Cidade obrigatória"),
   country: z.string().min(2, "País obrigatório"),
